7a85b080fb29f71e99df003fa1867fc1bee5494a
Oscar <>
2025-03-03 08:30:55 +0100

    add products

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,19 @@ export const toSlug = (text: string): string =>
     .replace(/\s+/g, "-")
     .replace(/^-+|-+$/g, "")
     .replace(/-+/g, "-");
+
+const CURRENCT_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+export function formatCurrency(amount: number) {
+  return CURRENCT_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
