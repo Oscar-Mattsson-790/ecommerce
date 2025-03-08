050e6a943fc770143d5be182b2d1807277a36fd9
Oscar <>
2025-03-08 10:06:52 +0100

    create browsing history

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -11,6 +11,8 @@ import ProductSlider from "@/components/shared/product/product-slider";
 import ProductGallery from "@/components/shared/product/product-gallery";
 import Rating from "@/components/shared/product/rating";
 import { Separator } from "@/components/ui/separator";
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
+import AddToBrowsingHistory from "@/components/shared/product/add-to-browsing-history";
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>;
@@ -48,6 +50,7 @@ export default async function ProductDetails(props: {
 
   return (
     <div>
+      <AddToBrowsingHistory id={product._id} category={product.category} />
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5 ">
           <div className="col-span-2">
@@ -118,6 +121,9 @@ export default async function ProductDetails(props: {
           products={relatedProducts.data}
           title={`Best Sellers in ${product.category}`}
         />
+      </section>
+      <section>
+        <BrowsingHistoryList className="mt-10" />
       </section>
     </div>
   );
