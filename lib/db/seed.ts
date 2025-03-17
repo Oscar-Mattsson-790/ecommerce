401d6e57170e7eaaadafe9b4e8c55923e4b295b0
Oscar <>
2025-03-17 07:17:09 +0100

    add signin user

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -3,20 +3,25 @@ import { connectToDatabase } from ".";
 import Product from "./models/product.model";
 import { loadEnvConfig } from "@next/env";
 import { cwd } from "process";
+import User from "./models/user.model";
 
 loadEnvConfig(cwd());
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 const main = async () => {
   try {
-    const { products } = data;
-    console.log("Connecting to MongoDB:", process.env.MONGODB_URI);
+    const { products, users } = data;
     await connectToDatabase(process.env.MONGODB_URI);
+    console.log("Connecting to MongoDB:", process.env.MONGODB_URI);
+
+    await User.deleteMany();
+    const createdUser = await User.insertMany(users);
 
     await Product.deleteMany();
     const createdProducts = await Product.insertMany(products);
 
     console.log({
+      createdUser,
       createdProducts,
       message: "Seeded database successfully",
     });
